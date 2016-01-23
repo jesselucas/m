6bcc35a4eebaabd94de515d8d3644cd2e99bfebe
Jesse Lucas <jesse@forestgiant.com>
2016-01-23 13:44:20 -0500

    Working on pre and post hooks to store cmds when in record mode.

--- cmd/m/.m.sh
+++ cmd/m/.m.sh
@@ -1,10 +1,67 @@
 #!/bin/bash
+M_RECORD=0
 m_command() {
+  # Call m and pass all param args
+  M_RECORD=0
+
+  # Record by setting up a bash trap
   if [ "$1" == "record" ]; then
-    m "$@" & # run m in background and pass all args
+    M_RECORD=1
+
+    # m "$@" & # run m in background and pass all args
+  fi
+
+  m "$@"
+}
+
+# This will run before any command is executed.
+m_pre() {
+  if [ -z "$M_AT_PROMPT" ]; then
     return
+  fi
+  unset M_AT_PROMPT
+
+  # echo $M_RECORD
+  echo "Pre hook"
+  if [ "$M_RECORD" -eq 1 ]; then
+    echo "Pre: Set M_CMD"
+    M_CMD=$*
   fi
 }
 
+trap 'm_pre $BASH_COMMAND' DEBUG
+
+# This will run after the execution of the previous full command line.  We don't
+# want post to execute when first starting a bash session (FIRST_PROMPT)
+M_FIRST_PROMPT=1
+m_post() {
+  M_AT_PROMPT=1
+
+  if [ -n "$M_FIRST_PROMPT" ]; then
+    unset M_FIRST_PROMPT
+    return
+  fi
+
+  local last_code=$?
+  if [ "$M_RECORD" -eq 1 ]; then
+    echo "post record"
+
+    # Don't add if the status errored
+    if [ "$last_code" -eq 0 ]; then
+      # Add current directory and command to r
+      m --add "$M_CMD"
+      echo "add $M_CMD"
+
+      return
+    fi
+  fi
+
+
+  echo "Post"
+}
+
+
+# Run post after command
+PROMPT_COMMAND="${PROMPT_COMMAND}"$'\n'"m_post;";
 
 alias m=m_command
--- cmd/m/main.go
+++ cmd/m/main.go
@@ -4,7 +4,6 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"time"
 
 	"github.com/jesselucas/m"
 )
@@ -13,6 +12,9 @@ func main() {
 	versionUsage := "Semantic Version of r"
 	versionPtr := flag.Bool("version", false, versionUsage)
 	flag.BoolVar(versionPtr, "v", false, versionUsage+" (shorthand)")
+
+	addUsage := "Adds a command to a recording"
+	addPtr := flag.String("add", "", addUsage)
 	flag.Parse()
 
 	// if the version flag is passed print the r package version and exit
@@ -21,17 +23,30 @@ func main() {
 		os.Exit(0)
 	}
 
+	// Setup new macro
+	macro := new(m.Macro)
+
+	// if the add flag is used
+	if *addPtr != "" {
+		macro.Add(*addPtr)
+		os.Exit(0)
+	}
+
 	mapCommands(os.Args[1:])
 }
 
 func mapCommands(args []string) {
+	if len(args) == 0 {
+		return
+	}
+
 	fmt.Println(args)
 
 	switch args[0] {
 	case "record":
 		fmt.Println("m: Commands are now being recorded.")
-		time.Sleep(5 * time.Second)
-		fmt.Println("finished sleep")
+		// time.Sleep(5 * time.Second)
+		// fmt.Println("finished sleep")
 	}
 
 	os.Exit(0)
